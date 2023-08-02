a4166e29e6ab64b1f8e11a743b393e91b3afa4e9
InnaUsenko <innausenko8@gmail.com>
2023-08-02 11:44:50 +0300

    Task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector("ul.gallery");
+const listItems = [];
+
+galleryItems.forEach((img) => {
+  const curItem = document.createElement("li");
+  curItem.classList.add("gallery__item");
+
+  const curLink = document.createElement("a");
+  curLink.setAttribute("href", img.original);
+  curLink.classList.add("gallery__link");
+
+  const curImg = document.createElement("img");
+  curImg.setAttribute("src", img.preview);
+  curImg.setAttribute("alt", img.description);
+  curImg.classList.add("gallery__image");
+
+  curLink.appendChild(curImg);
+  curItem.appendChild(curLink);
+  listItems.push(curItem);
+});
+list.append(...listItems);
+
+const options = {
+  captions: true,
+  captionsDelay: 250,
+  captionSelector: "img",
+  captionType: "attr",
+  captionPosition: "bottom",
+  captionsData: "alt",
+};
+const lightbox = new SimpleLightbox(".gallery a", options);
